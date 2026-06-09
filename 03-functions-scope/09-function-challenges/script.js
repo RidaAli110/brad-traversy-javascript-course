ce6e946cf805df5bf9661eb06d033ca669cf5905
RidaAli110 <rida.webdev110@gmail.com>
2026-06-09 22:36:52 +0100

    Add functions excersise

--- 03-functions-scope/09-function-challenges/script.js
+++ 03-functions-scope/09-function-challenges/script.js
@@ -14,3 +14,13 @@ function minMax(arr) {
   };
 };
 console.log(minMax([1, 2, 3, 4, 5, 6, 7]));
+function countEvenNumbers(arr) {
+  let count = 0;
+  for (const num of arr) {
+    if (num % 2 === 0) {
+      count++;
+    }
+  }
+  return count;
+}
+console.log(countEvenNumbers([1, 2, 3, 4, 5, 6]));
