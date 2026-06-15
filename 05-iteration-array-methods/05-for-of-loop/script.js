27e476f5537422aa60c081d583ec13b317511938
RidaAli110 <rida.webdev110@gmail.com>
2026-06-15 21:08:31 +0100

    Add for of loop practice

---
+++ 05-iteration-array-methods/05-for-of-loop/script.js
@@ -0,0 +1,23 @@
+// My code
+const colors = ['red', 'blue', 'green', 'yellow'];
+for (let color of colors) {
+     console.log(`color: ${color}`);
+}
+const numbers = [5, 10, 15, 20];
+for (let num of numbers) {
+     console.log(`Number: ${num}`);
+}
+const scores = [45, 80, 62, 90, 30];
+for (let score of scores) {
+     if (score >=60) {
+          console.log(score);
+     }
+}
+const colors2 = ['red', 'blue', 'green'];
+for (let color2 of  colors2) {
+     console.log(color2.toUpperCase());
+}
+const names = ['Ali', 'Sara', 'John', 'Fatima'];
+for (let name of names) {
+     console.log(`Hello ${name}!`);
+}
