bfdb26e9850f1775775ecc24a8a90443bd031855
RidaAli110 <rida.webdev110@gmail.com>
2026-06-15 17:01:49 +0100

    Add while-do-while loop practice

---
+++ 05-iteration-array-methods/03-while-do-while/script.js
@@ -0,0 +1,32 @@
+// My code following Brad Traversy's course
+let count = 1;
+while (count <= 10) {
+     console.log(count);
+     count++;
+}
+let count2 = 10;
+while (count2 >= 1) {
+     console.log(count2);
+     count2--;
+}
+   let i = 1;
+do {
+     console.log(i);
+i++;
+} while(i <= 5);
+
+let ind = 5;
+do {
+    console.log(`Number ${ind}`);
+    ind--;
+} while (ind >= 1);
+let even = 2;
+do {
+     console.log(`even number: ${even}`);
+     even += 2;                                                                                                                                                                                                                       
+} while (even <= 10);
+let y = 5;
+do {
+     console.log(y);
+     y += 5;
+} while (y < 31);
