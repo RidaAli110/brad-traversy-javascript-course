9ef265edb63fac2dd1a4a92f7d68588f249e6c5f
RidaAli110 <rida.webdev110@gmail.com>
2026-06-17 15:33:40 +0100

    Add array methods challenge

--- 05-iteration-array-methods/11-array-method-challenges/script.js
+++ 05-iteration-array-methods/11-array-method-challenges/script.js
@@ -36,6 +36,26 @@ const people = [
     age: 23,
   },
 ];
+const youngPeople = people.filter((person) => person.age <= 25)
+.map((person) => ({ 
+  name: person.firstName + ' ' + person.lastName,
+   email: person.email}))
+console.log(youngPeople);
 
 // Challenge 2
 const numbers = [2, -30, 50, 20, -12, -9, 7];
+const addEven = numbers.filter((num) => {
+  if (num > 0) {
+    return num;
+  }
+})
+.reduce((acc, curr) => acc + curr, 0)
+console.log(addEven);
+
+// cahllenge 3
+const words = ['coder', 'programmer', 'developer'];
+const capitalisedWords = words.map((word) => {
+  return word[0].toUpperCase() + word.slice(1)
+});
+console.log(capitalisedWords);
+
