caa701f31021c1d5b6d637d3f86350886721b0c1
RidaAli110 <rida.webdev110@gmail.com>
2026-07-21 21:13:58 +0100

    Add popular movies to display

--- 11-flix-app-project/flixx-app-theme/js/script.js
+++ 11-flix-app-project/flixx-app-theme/js/script.js
@@ -5,6 +5,53 @@ const global = {
   currentPage: window.location.pathname,
 };
 
+async function displayPopularMovies() {
+  const { results } = await fetchAPIData('movie/popular');
+  results.forEach((movie) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+          <a href="movie-details.html?id=${movie.id}">
+           ${
+             movie.poster_path
+               ? ` <img
+              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              class="card-img-top"
+              alt="${movie.title}"
+            />
+          </a>`
+               : ` <img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${movie.title}"
+            />
+          </a>`
+           }
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${movie.release_date}</small>
+            </p>
+          </div>
+        `;
+    // Add to DOM
+    document.querySelector('#popular-movies').appendChild(div);
+  });
+}
+
+//  fetch data from TMDB API
+async function fetchAPIData(endpoint) {
+  const API_KEY = '9845c998380f98087e43647a19bc08b3';
+  const API_URL = 'https://api.themoviedb.org/3/';
+
+  const response = await fetch(
+    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-us`,
+  );
+  const data = await response.json();
+
+  return data;
+}
+
 // Highlight active link
 function highlightActiveLink() {
   const navLinks = document.querySelectorAll('.nav-link');
@@ -21,6 +68,7 @@ function init() {
   switch (global.currentPage) {
     case '/':
     case '/index.html':
+      displayPopularMovies();
       console.log('home');
       break;
     case '/shows.html':
