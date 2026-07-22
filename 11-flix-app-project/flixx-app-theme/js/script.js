297487a0c6496cc6319b9d171a97a9a08156ef02
RidaAli110 <rida.webdev110@gmail.com>
2026-07-22 21:43:55 +0100

    Add slider swiper

--- 11-flix-app-project/flixx-app-theme/js/script.js
+++ 11-flix-app-project/flixx-app-theme/js/script.js
@@ -211,6 +211,48 @@ function displayBackgroundImage(type, backgroundPath) {
   }
 }
 
+// Display slider movies
+async function displaySlider() {
+  const { results } = await fetchAPIData('movie/now_playing');
+
+  results.forEach((movie) => {
+    const sliderDiv = document.createElement('div');
+    sliderDiv.classList.add('swiper-slide');
+    sliderDiv.innerHTML = `<a href="movie-details.html?id=${movie.id}">
+              <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+            </a>
+            <h4 class="swiper-rating">
+              <i class="fas fa-star text-secondary"></i> ${movie.vote_average.toFixed(1)} / 10
+            </h4>`;
+    document.querySelector('.swiper-wrapper').appendChild(sliderDiv);
+
+    initSwiper();
+  });
+}
+
+function initSwiper() {
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+      },
+      700: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+    },
+  });
+}
 //  fetch data from TMDB API
 async function fetchAPIData(endpoint) {
   // Only use this key for development or very small projects. You should store your key and make requests from server
@@ -256,8 +298,8 @@ function init() {
   switch (global.currentPage) {
     case '/':
     case '/index.html':
+      displaySlider();
       displayPopularMovies();
-      console.log('home');
       break;
     case '/shows.html':
       displayPopularShows();
