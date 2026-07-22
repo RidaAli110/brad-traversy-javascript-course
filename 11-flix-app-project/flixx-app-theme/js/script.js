b1471a731ef58b6c4a691522d92b6ede88a8f8de
RidaAli110 <rida.webdev110@gmail.com>
2026-07-22 19:52:53 +0100

    Add Popular TV shows / movies details page/ details backdrop and spinner

--- 11-flix-app-project/flixx-app-theme/js/script.js
+++ 11-flix-app-project/flixx-app-theme/js/script.js
@@ -1,10 +1,11 @@
 // My code following Brad's course
 
-// // Stores the current page's path so the router knows which page is open
+// Stores the current page's path so the router knows which page is open
 const global = {
   currentPage: window.location.pathname,
 };
 
+// Display popular movies
 async function displayPopularMovies() {
   const { results } = await fetchAPIData('movie/popular');
   results.forEach((movie) => {
@@ -39,17 +40,144 @@ async function displayPopularMovies() {
   });
 }
 
+// Display popular TV shows
+async function displayPopularShows() {
+  const { results } = await fetchAPIData('tv/popular');
+  results.forEach((show) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+          <a href="tv-details.html?id=${show.id}">
+           ${
+             show.poster_path
+               ? ` <img
+              src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+              class="card-img-top"
+              alt="${show.name}"
+            />
+          </a>`
+               : ` <img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${show.name}"
+            />
+          </a>`
+           }
+          <div class="card-body">
+            <h5 class="card-title">${show.name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${show.first_air_date}</small>
+            </p>
+          </div>
+        `;
+    // Add to DOM
+    document.querySelector('#popular-shows').appendChild(div);
+  });
+}
+
+// Display movie details
+async function displayMovieDetails() {
+  const movieId = window.location.search.split('=')[1];
+
+  const movie = await fetchAPIData(`movie/${movieId}`);
+  //Overlay for background image
+  displayBackgroundImage('movie', movie.backdrop_path);
+
+  const div = document.createElement('div');
+  div.innerHTML = `        <div class="details-top">
+          <div>
+            <${
+              movie.poster_path
+                ? ` <img
+              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              class="card-img-top"
+              alt="${movie.title}"
+            />
+          </a>`
+                : ` <img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${movie.title}"
+            />
+          </a>`
+            }
+          </div>
+          <div>
+            <h2>${movie.title}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${movie.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Release Date: ${movie.release_date}</p>
+            <p>${movie.overview}</p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+            ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+            </ul>
+            <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Movie Info</h2>
+          <ul>
+            <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(movie.budget)}</li>
+            <li><span class="text-secondary">Revenue:</span>$${addCommasToNumber(movie.revenue)}</li>
+            <li><span class="text-secondary">Runtime:</span> ${movie.runtime} minutes</li>
+            <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">
+          ${movie.production_companies.map((company) => `<span>${company.name}</span>`).join(' - ')}
+          </div>
+        </div>`;
+  document.querySelector('#movie-details').appendChild(div);
+}
+
+// Display backdrop on details pages
+function displayBackgroundImage(type, backgroundPath) {
+  const overlayDiv = document.createElement('div');
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+  overlayDiv.style.backgroundSize = 'cover';
+  overlayDiv.style.backgroundPosition = 'center';
+  overlayDiv.style.backgroundRepeat = 'no-repeat';
+  overlayDiv.style.height = '100vh';
+  overlayDiv.style.width = '100vw';
+  overlayDiv.style.position = 'absolute';
+  overlayDiv.style.top = '0';
+  overlayDiv.style.left = '0';
+  overlayDiv.style.zIndex = '-1';
+  overlayDiv.style.opacity = '0.1';
+  if (type === 'movie') {
+    document.querySelector('#movie-details').appendChild(overlayDiv);
+  } else {
+    document.querySelector('#show-details').appendChild(overlayDiv);
+  }
+}
+
 //  fetch data from TMDB API
 async function fetchAPIData(endpoint) {
+  // Only use this key for development or very small projects. You should store your key and make requests from server
   const API_KEY = '9845c998380f98087e43647a19bc08b3';
   const API_URL = 'https://api.themoviedb.org/3/';
+
+  showSpinner();
 
   const response = await fetch(
     `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-us`,
   );
   const data = await response.json();
 
+  hideSpinner();
+
   return data;
+}
+
+function showSpinner() {
+  document.querySelector('.spinner').classList.add('show');
+}
+
+function hideSpinner() {
+  document.querySelector('.spinner').classList.remove('show');
 }
 
 // Highlight active link
@@ -63,6 +191,9 @@ function highlightActiveLink() {
   });
 }
 
+function addCommasToNumber(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
 // Initialise app -  Checks which page is open and runs the required functions
 function init() {
   switch (global.currentPage) {
@@ -72,10 +203,10 @@ function init() {
       console.log('home');
       break;
     case '/shows.html':
-      console.log('shows');
+      displayPopularShows();
       break;
     case '/movie-details.html':
-      console.log('movie details');
+      displayMovieDetails();
       break;
     case '/tv-details.html':
       console.log('tv details');
